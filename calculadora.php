564899dad7806117cbe38daf5e09af4ea07c4d49
dvctrl <davidmouzinho.contato@gmail.com>
2023-03-01 20:13:51 -0300

    exercicio calculadora

---
+++ calculadora.php
@@ -0,0 +1,13 @@
+<?php  
+
+    echo "Calculadora soma <br/>";
+
+    $a = 12;
+    $b = 22;
+    $c = $a + $b;
+    $resultado =  sprintf("a soma dos valores $a e $b é: %.0f ",$c);
+    
+    echo $resultado;
+
+
+?>
