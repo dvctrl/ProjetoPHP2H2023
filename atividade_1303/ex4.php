9c73dbbd4ac7c5f86136a874127e5cc0e858660e
dvctrl <davidmouzinho.contato@gmail.com>
2023-03-13 20:17:18 -0300

    atividade 4

--- atividade_1303/ex4.php
+++ atividade_1303/ex4.php
@@ -1,5 +1,21 @@
 <?php
+    $a=5;
+    $b=4;
+    $c=6;
 
-  
+    if ($a > $b && $c){
+        if ($b > $c){
+            echo "1.1 a ordem é-> ".$a.",".$b.",".$c;
+        } else {
+            echo "1.2 a ordem é-> ".$a.",".$c.",".$b;
+        }
+    }else if ($b > $a && $c){
+        if ($a > $c){
+            echo "2.1 a ordem é-> ".$b.",".$a.",".$c;
+        } else {
+            echo "2.2 a ordem é-> ".$b.",".$c.",".$a;
+        }
+    }
+    
     
 ?>
