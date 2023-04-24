d2776a3a0bf549b26b91d127d9e389373ffc30fa
dvctrl <davidmouzinho.contato@gmail.com>
2023-04-24 19:22:15 -0300

    array matriz

---
+++ atividade_2404/arrayMatriz.php
@@ -0,0 +1,20 @@
+<?php
+    $emp = array(
+        array(1,"José", 4000),
+        array(2,"Maria",5000),
+        array(3,"João",3000)
+    );
+
+    print_r($emp);
+
+?>
+<?php
+    $func = array(
+        array(1,"Francisca",4000),
+        array(2,"Ponto gê",1000),
+        array(3,"Debis",1000)
+    );
+
+    print_r($func);
+
+?>
