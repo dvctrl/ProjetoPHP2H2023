1c5a36f8625ad9ea560345aab281234e06059b45
dvctrl <davidmouzinho.contato@gmail.com>
2023-04-24 19:53:38 -0300

    exemplo matriz

--- atividade_2404/arrayMatriz.php
+++ atividade_2404/arrayMatriz.php
@@ -4,9 +4,9 @@
         array(2,"Maria",5000),
         array(3,"João",3000)
     );
-
+    echo "<pre>";
     print_r($emp);
-
+    echo "</pre>";
 ?>
 <?php
     $func = array(
@@ -14,7 +14,22 @@
         array(2,"Ponto gê",1000),
         array(3,"Debis",1000)
     );
-
+    echo "<pre>";
     print_r($func);
+    echo "</pre>";
+?>
+<?php
+    $emp = array(
+        array(1,"José", 4000),
+        array(2,"Maria",5000),
+        array(3,"João",3000)
+    );
+    for($linha=0; $linha<3; $linha++) {
+        for($coluna=0; $coluna<3; $coluna++) {
+            echo $emp [$linha][$coluna]." ";
+        }
+        echo "<br/>";
+    } 
+
 
 ?>
