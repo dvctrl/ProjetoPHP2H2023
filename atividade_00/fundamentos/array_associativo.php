4fde0812df19d7ab101a50c371a8a54553fa16a3
David Santos Mouzinho <davidmouzinho.contato@gmail.com>
2023-04-25 14:05:43 -0300

    array functions

---
+++ atividade_00/fundamentos/array_associativo.php
@@ -0,0 +1,13 @@
+<?php
+    $v = array(1 => 'a', 3 => 'b', 6 => 'c', 8 => 'd');
+
+    print_r($v);
+    /* esta é uma forma onde posso customizar a chave, relacionando-a com um valor.  */
+?>
+
+<?php
+    $v = array("MA" => 'Maria', "VI" => 'Vitor', "DA" => "David");
+
+    print_r("$v")
+
+?>
