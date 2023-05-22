a1a58c77e7fb49471c6c1e3695b9d03b62c9a03c
dvctrl <davidmouzinho.contato@gmail.com>
2023-05-22 20:15:06 -0300

    formulario feito pela debs

--- Marco/formularioSalario.php
+++ Marco/formularioSalario.php
@@ -8,31 +8,19 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
     <title>Document</title>
 </head>
-<body style=" margin: 100px;">  
-    
-        <div class="card" style=" width: 18rem;">
-            <img class= "card-img-top" src= "img/calculadora.png" style="widht: 30rem;">
-                <div class="card-body">
-                    <form action="calculadora.php" method="get">
-                        <div class="form-group">
-                    <label for="numero1"> Insira o primeiro numero</label>
-                    <input type="number" class="form-control" name="numero1">
-                        </div>
-                <div class="form-group">
-                    <label for="numero2"> Insira o segundo numero</label>
-                    <input type= "number" class="form-control"  name= "numero2">
-                </div>
-                    
-                    <button type="submit" class="btn btn-outline-info">Calcular</button>
-                  
-        </form>
-</div>
-</div>
-        
+<body>
+    <form action="exerDois.php" method="get">
+        <label> Quantidade de horas trabalhadas?</label>
+        <input type="number" name="">
+
+        <label>Quanto você recebe por hora?</label>
+        <input type="number" name="">
+
+        <label>Quantos dependentes você tem?</label>
+        <input type="number" name="">
+    </form>
     
 </body>
 </html>
-
